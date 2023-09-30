76b57e192df248f1bb7ed909e3b12b97a1194b71
La Loutre Cosmique <sushi.laque77@gmail.com>
2023-09-30 11:57:38 +0200

    create ore database

--- Assets/Scripts/Ores/Ore.cs
+++ Assets/Scripts/Ores/Ore.cs
@@ -7,6 +7,7 @@ public class Ore : ScriptableObject
     public Sprite icon;
 
     public new string name;
-    public int rarity; // 1 = super rare, 30 = rare, 100 = common;
+    public int hardness;
+    public int rarity;
     public int price;
 }
--- Assets/Scripts/Ores/OresGeneration.cs
+++ Assets/Scripts/Ores/OresGeneration.cs
@@ -1,6 +1,45 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class OresGeneration : MonoBehaviour
 {
-    
+    [SerializeField] OreDataBase oreDataBase;
+
+    [SerializeField] [Range(0.0f,1.0f)] float rockApparitionPercentage;
+
+    void Awake()
+    {
+        oreDataBase = GetComponent<OreDataBase>();
+    }
+
+    void Start()
+    {
+        foreach (var ore in oreDataBase.oreList)
+        {
+            Debug.Log(ore.name);
+        }
+    }
+
+    /*void Update()
+    {
+        Debug.Log(GetRock());
+        
+        if (GetRock())
+            Debug.Log("CAILLOUX");
+        else
+            Debug.Log(GetRandomOre().name);
+    }*/
+
+    // Return true if create rock
+    bool GetRock()
+    {
+        float result = Random.Range(0f, 1f);
+        return result <= rockApparitionPercentage;
+    }
+
+    public Ore GetRandomOre()
+    {
+        return null;
+    }
 }
